56f4f080747874acff5481bb3a3d87e0ebc91661
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2021-05-26 09:33:28 +0200

    Prepare the screens, structure and loading fonts

---
+++ README.md
@@ -0,0 +1,17 @@
+# Recipe-ist
+
+A very recipe management tool.
+
+# Run
+The application uses expo.io, install it https://docs.expo.io/.
+
+```
+$ npm start
+```
+
+# HowTo's
+
+Importing expo-fonts in the project
+```
+npm install --save expo-font
+```
---
+++ screens/DetailScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const DetailScreen = (props) => {
+  <View style={styles.screen}>
+    <Text>The Details Screen</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default DetailScreen;
